0c84e2cd556aea54a344290377462a6b6ead101d
Your Name <you@example.com>
2022-08-10 12:09:04 +0530

    [Megha] Add: Added UC1

--- Program.cs
+++ Program.cs
@@ -5,6 +5,10 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("***Welcome To Find Maximum Numbe***");
+            MaximumIntegerNum maximum = new MaximumIntegerNum();
+            int value = maximum.MaximumInteger(44, 56, 72);
+
+            Console.WriteLine("The Maximum Integer is:- " + value);
         }
     }
 }
