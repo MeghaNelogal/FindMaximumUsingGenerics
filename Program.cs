26703d85237e49d00f9b7705b62f18e11d88d368
Your Name <you@example.com>
2022-08-10 12:18:34 +0530

    [Megha] Add: Added UC2

--- Program.cs
+++ Program.cs
@@ -4,11 +4,16 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("***Welcome To Find Maximum Numbe***");
-            MaximumIntegerNum maximum = new MaximumIntegerNum();
-            int value = maximum.MaximumInteger(44, 56, 72);
+            //Console.WriteLine("***Welcome To Find Maximum Numbe***");
+            //MaximumIntegerNum maximum = new MaximumIntegerNum();
+            //int value = maximum.MaximumInteger(44, 56, 72);
 
-            Console.WriteLine("The Maximum Integer is:- " + value);
+            //Console.WriteLine("The Maximum Integer is:- " + value);
+
+            FindMaximumFloat maximum = new FindMaximumFloat();
+            double value = maximum.MaximumFloatNumber(3.6, 5.7, 7.7);
+
+            Console.WriteLine("The Maximum Float is:- " + value);
         }
     }
 }
